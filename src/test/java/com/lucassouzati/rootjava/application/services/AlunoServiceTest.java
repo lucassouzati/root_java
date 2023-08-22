51936443ff9b308556e68757306265b7c534f545
Lucas de Souza Siqueira <lucassouza.ti@gmail.com>
2023-08-22 06:53:17 -0300

    test: criado teste para create de aluno service

---
+++ src/test/java/com/lucassouzati/rootjava/application/services/AlunoServiceTest.java
@@ -0,0 +1,28 @@
+package com.lucassouzati.rootjava.application.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import com.lucassouzati.rootjava.application.dto.InputAlunoDto;
+import com.lucassouzati.rootjava.application.dto.OutputAlunoDto;
+import com.lucassouzati.rootjava.infrasctructure.repositories.InMemoryAlunoRepository;
+
+public class AlunoServiceTest {
+
+    @Test
+    public void deveCriarAlunoComSucesso() {
+
+        var inputAlunoDto = new InputAlunoDto("teste", 100);
+        var alunoService = new AlunoService(new InMemoryAlunoRepository());
+
+        OutputAlunoDto outputAlunoDto = alunoService.create(inputAlunoDto);
+
+        assertEquals(inputAlunoDto.nome(), outputAlunoDto.nome());
+        assertEquals(inputAlunoDto.notaFinal(), outputAlunoDto.notaFinal());
+        assertNotNull(outputAlunoDto.id());
+
+    }
+
+}
--- target/classes/db/migrations/migration.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE aluno (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    nota_final DOUBLE DEFAULT 0
-);
